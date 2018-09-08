9fd4bd3e12d71ced59a7e96ee31399033852c555
incybro <incybro@gmail.com>
2018-09-08 11:09:26 +0200

    Added post_detail view

--- Homepage/views.py
+++ Homepage/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 from django.utils import timezone
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'Homepage/post_list.html', {'posts': posts})
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'Homepage/post_detail.html', {'post': post})
