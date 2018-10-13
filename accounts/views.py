fd32c3e0a74d5b3dd77ff5ebdbe660da6e863af0
incybro <incybro@gmail.com>
2018-10-13 13:46:53 +0200

    Added post edit form

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.models import User
-from django.shortcuts import render, get_object_or_404
-from .models import UserProfile
-
+from django.shortcuts import render, get_object_or_404, redirect
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
